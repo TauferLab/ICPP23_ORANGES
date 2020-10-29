9418949d7015dd668f555babf89a68b25ec37c5f
Nick Bell <pnbell@tellico-master0.local>
2020-10-29 17:25:50 -0400

    did a few more scaling tests and automated using multiple process counts

--- GRAAL/parallel_graph_align/lsf_graph_align.sh
+++ GRAAL/parallel_graph_align/lsf_graph_align.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
-n_procs=$1
-input_graph=$2
+#n_procs=$1
+input_graph=$1
 
 root_path=$HOME/Src_GraphAlignment/GRAAL
 
@@ -10,4 +10,9 @@ graph_align_job_script=${root_path}/parallel_graph_align/graph_align_par.sh
 output_file=${root_path}/debugging/lsf_output.txt
 error_file=${root_path}/debugging/lsf_error.txt
 
-bsub > ${output_file} 2> ${error_file} -n ${n_procs} ${graph_align_job_script} ${n_procs} ${input_graph}
+run_scales=(32)
+
+for n_procs in ${run_scales[@]};
+do
+    bsub -o ${output_file} -e ${error_file} -n ${n_procs} ${graph_align_job_script} ${n_procs} ${input_graph}
+done
