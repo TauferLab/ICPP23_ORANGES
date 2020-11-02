974063236bbd392263ed51945498db6c4ec15499
Nick Bell <pnbell@tellico-master0.local>
2020-11-02 15:34:18 -0500

    parallel shell scripts updated to make multiple runs based on configuration

--- GRAAL/parallel_graph_align/graph_align_par.sh
+++ GRAAL/parallel_graph_align/graph_align_par.sh
@@ -3,9 +3,10 @@
 #BSUB -o build_graph-%j.out                                                                                                                                                                                   
 #BSUB -e build_graph-%j.err                                                                                                                                                                           
 
-n_procs=$1
-input_graph=$2
+#n_procs=$1
+#input_graph=$2
 #graph_alignment_bin=$2
+n_runs=$1
 
 root_path=$HOME/Src_GraphAlignment/GRAAL
 
@@ -23,7 +24,23 @@ cd ${root_path}/parallel_graph_align
 make clean
 make all
 
-echo "======================================================================"
-echo "Starting run of ${input_graph} on ${n_procs} processes"
-mpirun --oversubscribe -np ${n_procs} ./graph_alignment ${graph_folder}/${input_graph} ${orbit_file} -o ${std_out} -e ${std_err}
-echo "======================================================================"
+# Configure Inputs                     
+run_scales=(4 8 16 32 64)
+input_graphs=(graph_slice3.txt)
+run_idx_low=1
+run_idx_high=${n_runs}
+
+ 
+for input_graph in ${input_graph[@]};
+do
+    for n_procs in ${run_scales[@]};
+    do
+        for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`;
+        do
+	    echo "======================================================================"
+	    echo "Starting run ${run_idx} of ${input_graph} on ${n_procs} processes"
+	    mpirun --oversubscribe -np ${n_procs} ./graph_alignment ${graph_folder}/${input_graph} ${orbit_file} -o ${std_out} -e ${std_err}
+	    echo "======================================================================"
+	done
+    done
+done
--- GRAAL/parallel_graph_align/lsf_graph_align.sh
+++ GRAAL/parallel_graph_align/lsf_graph_align.sh
@@ -13,20 +13,20 @@ output_file=${root_path}/debugging/lsf_output.txt
 error_file=${root_path}/debugging/lsf_error.txt
 
 # Configure Inputs
-run_scales=(4 8 16 32 64)
-input_graphs=(graph_slice3.txt)
-run_idx_low=1
-run_idx_high=${n_runs}
+#run_scales=(4 8 16 32 64)
+#input_graphs=(graph_slice3.txt)
+#run_idx_low=1
+#run_idx_high=${n_runs}
 
 #for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`; 
 #do
-for input_graph in ${input_graph[@]};
-do
-    for n_procs in ${run_scales[@]};
-    do
-	for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`;
-	do
-	    bsub -o ${output_file} -e ${error_file} -n ${n_procs} ${graph_align_job_script} ${n_procs} ${input_graph}
-	done
-    done
-done
+#for input_graph in ${input_graph[@]};
+#do
+#    for n_procs in ${run_scales[@]};
+#    do
+#	for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`;
+#	do
+bsub -o ${output_file} -e ${error_file} -n ${n_procs} ${graph_align_job_script} ${n_runs}
+#	done
+#    done
+#done
