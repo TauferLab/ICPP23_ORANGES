26f0abdb01d61d670f3d6669caa4af2ee7bef947
Nick Bell <tg873913@login4.stampede2.tacc.utexas.edu>
2021-10-27 16:45:35 -0500

    Create updated strong scale vis script for new runtime format

---
+++ fido/visualization/vis_strong_speedup.py
@@ -0,0 +1,152 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+import argparse
+
+def main( filepath , run_min , run_max ):
+
+    #runtimes_prior_gather_2d = []*int(run_max);
+    #runtimes_post_gather_2d = []*int(run_max);
+    #runtimes_total_2d = []*int(run_max);
+    run_scales = [2, 4, 8, 16, 32]
+    n_runs = (int(run_max)+1)-int(run_min)
+    runtime_data = np.zeros((n_runs , len(run_scales)))
+
+    run_index = 0
+    proc_index = 0
+
+    for run_num in range(int(run_min), int(run_max)+1):
+        for n_procs in run_scales:
+
+            if (run_num < 10):
+                filename = filepath + "/run_00" + str(run_num) + "/procs_" + str(n_procs) + "/runtimes_data/runtimes_rec_over.txt";
+            if (run_num >= 10 and run_num < 100):
+                filename = filepath + "/run_0" + str(run_num) + "/procs_" + str(n_procs) + "/runtimes_data/runtimes_rec_over.txt";
+            with open(filename, "r") as file:
+
+                #runtimes_prior_gather = [];                                                                                                                                                                                              
+                #runtimes_post_gather = [];
+                #runtimes_total = [];
+
+                count_line = 0;
+                for line in file:
+                    if (count_line != 0):
+                        count_word = 0;
+                        #runtimes_prior_gather = [];
+                        #runtimes_post_gather = [];
+                        #runtimes_total = [];
+                        for word in line.split():
+                            if (count_word == 1):
+                                #runtimes_prior_gather.append(float(word));
+                                #runtime_data[ run_index , proc_index ] = 
+                            #if (count_word == 2):
+                            #    runtimes_post_gather.append(float(word));
+                            #if (count_word == 3):
+                            #    runtimes_total.append(float(word));
+                            if (count_word == 5):
+                                runtime_data[ run_index , proc_index ] = float(word)
+                            count_word += 1;
+                    count_line += 1;
+                #runtimes_prior_gather_2d.append(runtimes_prior_gather);
+                #runtimes_post_gather_2d.append(runtimes_post_gather);
+                #runtimes_total_2d.append(runtimes_total);
+
+            proc_index += 1
+        run_index += 1
+
+    #print(runtimes_total);
+    #print(runtimes_prior_gather);
+    #print(runtimes_post_gather);
+
+    #x_vals = np.array(range(0, len(runtimes_total)));
+    #print(np.shape(x_vals))
+    #prior_gather = np.array(runtimes_prior_gather_2d);
+    #post_gather = np.array(runtimes_post_gather_2d);
+    #total_time = np.array(runtimes_total_2d);
+
+    print(np.shape(runtime_data))
+    print(runtime_data)
+    #print(np.shape(prior_gather));
+    ax = 0;
+    #prior_gather_avg = np.mean(prior_gather, axis=ax);
+    #post_gather_avg = np.mean(post_gather, axis=ax);
+    #total_time_avg = np.mean(total_time, axis=ax);
+    #x_vals = np.array(range(0, len(runtimes_total)));
+    x_vals = run_scales
+    #print(total_time_avg);
+    #gather_addition = np.subtract(post_gather_avg, prior_gather_avg);
+    #print(gather_addition)
+    #print(x_vals)
+
+    #fig1 = plt.figure();
+    #plt.bar(x_vals, prior_gather_avg, color='b');
+    #plt.bar(x_vals, gather_addition, bottom=prior_gather_avg, color='r');
+    #plt.xticks(x_vals);
+
+    #plt.xlabel('MPI Rank x')
+    #plt.ylabel('Runtime (s.) on MPI Rank x')
+    #plt.legend(['Prior Gather', 'Post Gather']);
+    #plt.axis('tight')
+    #plt.title('Time Taken on Each Rank of GDV Vector Calculation');
+    #plt.savefig( "png_files/load_imb_bar_vec.png", bbox_inches="tight", pad_inches=0.25);
+
+
+    fig2 = plt.figure()
+    #plt.bar(x_vals, prior_gather_avg, color='b');
+    #plt.boxplot(prior_gather, positions=x_vals);
+    plt.violinplot(data)
+    plt.xticks(x_vals)
+
+    plt.xlabel('MPI Rank x')
+    plt.ylabel('Runtime (s.) on MPI Rank x')
+    #plt.legend(['Prior Gather', 'Post Gather']);
+    #plt.axis('tight')
+    #plt.ylim([0, 75]);
+    plt.xticks(np.arange(0, len(runtimes_total), step=2), np.arange(0, len(runtimes_total), step=2));
+    plt.title('Time Taken on Each Rank of GDV Vector Calculation Prior to MPI_Gather');
+    plt.savefig( "png_files/load_imb_box_prior_gather.png", bbox_inches="tight", pad_inches=0.25);
+
+
+    #fig3 = plt.figure();
+    #plt.bar(x_vals, post_gather_avg, color='b');
+    #plt.xticks(x_vals);
+
+    #plt.xlabel('MPI Rank x')
+    #plt.ylabel('Runtime (s.) on MPI Rank x')
+    ##plt.legend(['Prior Gather', 'Post Gather']);
+    #plt.axis('tight')
+    #plt.title('Time Taken on Each Rank of GDV Vector Calculation');
+    #plt.savefig( "png_files/load_imb_bar_post_gather.png", bbox_inches="tight", pad_inches=0.25);
+
+
+    #fig4 = plt.figure();
+    #plt.bar(x_vals, total_time_avg, color='b');
+    ##plt.bar(x_vals, post_gather, bottom=prior_gather);
+    #plt.xticks(x_vals)
+
+    #plt.xlabel('MPI Rank x')
+    #plt.ylabel('Runtime (s.) on MPI Rank x')
+    #plt.axis('tight')
+    #plt.title('Time Taken on Each Rank of Full Similarity Calculation');
+    #plt.savefig( "png_files/load_imb_bar_full.png", bbox_inches="tight", pad_inches=0.25);
+
+
+if __name__ == "__main__":
+    desc = "Generates figure showing runtime load imbalance data"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("filepath",
+                        help="Path to run directories storing runtime data")
+    parser.add_argument("-rn", "--run_min", required=True,
+                        help="Starting run directory index")
+    parser.add_argument("-rx", "--run_max", required=True,
+                        help="Final run directory index")
+    args = parser.parse_args()
+    main( args.filepath, args.run_min, args.run_max )
+
+
+
+
+
+
