719fd0e7e86bd0658debfec74a65215a935b9da4
git-tea <pbell12@utk.edu>
2021-12-09 15:54:18 -0600

    Patch compile options to move out num_threads

--- install/install_fido.sh
+++ install/install_fido.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 track_runtime=$1
-num_threads=$2
+#num_threads=$2
 sim_mat_cutoff=$3
 
 build_dir=build
@@ -16,7 +16,7 @@ cd ${build_dir}
 #kokkos_kernels_path=$2
 
 track_runtime_cmake="${track_runtime_cmake:="1"}"
-num_threads_cmake="${num_threads:="1"}"
+#num_threads_cmake="${num_threads:="1"}"
 sim_mat_cutoff="${sim_mat_cutoff:="100"}"
 
 export CC=mpicc
@@ -25,7 +25,7 @@ cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_CXX_COMPILER=mpicxx \
   -DTRACK_RUNTIME:STRING=${track_runtime_cmake} \
-  -DTHREAD_COUNT:STRING=${num_threads_cmake} \
+  #-DTHREAD_COUNT:STRING=${num_threads_cmake} \
   -DSIM_MAT_CUT:STRING=${sim_mat_cutoff} \
   -DCMAKE_PREFIX_PATH=${project_root} \
   -DKokkos_DIR=${project_root}/submodules/kokkos/${build_dir}/${sys_lib}/cmake/Kokkos \
